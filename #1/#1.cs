3799ff12a36b8f0a1b6229d3b4afdb9309dc3131
VincentOpiana-BSCS21A1 <vincent012opiana@gmail.com>
2024-10-12 11:56:43 +0800

    Add files via upload

---
+++ #1/#1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ConsoleApp1.Exam
+{
+    // Bank Account Class
+    public class BankAccount
+    {
+        private decimal balance;
+
+        public decimal Balance
+        {
+            get { return balance; }
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($" Deposited         : {amount:C}.");
+            }
+            else
+            {
+                Console.WriteLine("Deposit amount must be positive.");
+            }
+        }
+
+        public string Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance -= amount;
+                return $" Withdrew          : {amount:C}\n Remaining balance : {balance:C}.";
+            }
+            else if (amount > balance)
+            {
+                return "Insufficient funds.";
+            }
+            else
+            {
+                return "Withdrawal amount must be positive.";
+            }
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+  
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine();
+            Console.WriteLine(" Name : Vincent Lloyd M. Opiana ");
+            Console.WriteLine();
+            Console.WriteLine(" ======== Bank account ======== ");
+            Console.WriteLine();
+
+    
+            account.Deposit(100); 
+            Console.WriteLine(account.Withdraw(30));
+
+
+
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+
+    }
+}
